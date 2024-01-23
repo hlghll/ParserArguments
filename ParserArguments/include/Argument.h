379611fda54adefc329884000c540b1a24d8c6ef
hlghll <153762141+hlghll@users.noreply.github.com>
2024-01-23 13:19:55 +0300

    Update Argument.h

--- ParserArguments/include/Argument.h
+++ ParserArguments/include/Argument.h
@@ -14,10 +14,10 @@ const int kDefaultValueMinArgs = 1;
 class BaseArgument {
 public:
 	bool has_default_value;
-    bool has_store_value;
-    bool has_multi_value;
-    bool is_positional;
-    bool has_value;
+	bool has_store_value;
+	bool has_multi_value;
+	bool is_positional;
+	bool has_value;
 	char short_name;
 	std::string_view long_name;
 	std::string_view description;
@@ -34,23 +34,22 @@ template <typename Type>
 class Argument : public BaseArgument {
 public:
 	Argument() = default;
-    ~Argument() = default;
+	~Argument() = default;
 	Argument(const char short_name, const char* long_name, const char* description);
 	Argument& Positional();
-    Argument& Default(Type value);
-    Argument& StoreValue(Type& link_value);
-    Argument& MultiValue(int min_args_counts = kDefaultValueMinArgs);
-    Argument& StoreValues(std::vector<Type>& link_value);
-    Argument& SetValue(Type value);
-    Type GetValue(int index = kDefaultValueMinArgs);
-    size_t GetSizeMultiValue();
+	Argument& Default(Type value);
+	Argument& StoreValue(Type& link_value);
+    	Argument& MultiValue(int min_args_counts = kDefaultValueMinArgs);
+	Argument& StoreValues(std::vector<Type>& link_value);
+	Argument& SetValue(Type value);
+	Type GetValue(int index = kDefaultValueMinArgs);
+	size_t GetSizeMultiValue();
 
 private:
 	int min_args_counts_;
 	Type value_;
-    Type default_value_;
-    Type* store_value_;
-    std::vector<Type> multi_value_;
-    std::vector<Type>* multi_store_value_;
-
+	Type default_value_;
+	Type* store_value_;
+	std::vector<Type> multi_value_;
+	std::vector<Type>* multi_store_value_;
 };
